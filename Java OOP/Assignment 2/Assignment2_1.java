15e1f612e0cff6da562f23d77425f700ac2cf54a
adimitrova <a.dimitrovaa@gmail.com>
2016-11-22 21:46:02 +0100

    ass.2 beginning

--- Java OOP/Assignment 2/Assignment2_1.java
+++ Java OOP/Assignment 2/Assignment2_1.java
@@ -1,3 +1,4 @@
+import javax.swing.text.AbstractDocument.LeafElement;
 
 public class Assignment2_1 {
 
@@ -28,14 +29,60 @@ public class Assignment2_1 {
 	
 	public static void rotate(int[] seq){
 		int[] origArr = seq;
-		int[] shiftedArr = new int[seq.length];
+		int[] shiftedArrL1 = new int[seq.length];
+		int[] shiftedArrL2 = new int[seq.length];
+		int[] shiftedArrR = new int[seq.length];
 		
-		for (int i = 0; i <= origArr.length-1; i++) {
-			for (int j = i+1; j < shiftedArr.length; j++) {
-				shiftedArr[j] = origArr[i];
-				System.out.print(shiftedArr[j] + " ");
+		System.out.print("Original array:	");
+		System.out.println(java.util.Arrays.toString(origArr));
+		
+		// LEFT SHIFT 1st VERSION
+		for (int i = 0, j = 1; i < origArr.length-1; i++, j++) {
+			// left shift
+			// origArr [0] [1] [2] [3] j
+			// copyArr [1] [2] [3] [0] i
+			
+			shiftedArrL1[i] = origArr[j];
+				
+			if(j == origArr.length-1){
+				shiftedArrL1[origArr.length-1] = origArr[0];
 			}
 		}
+		
+		// LEFT SHIFT 2nd VERSION
+		for (int i = origArr.length-1, j = i-1; i >= 0; i--, j--) {
+			// left shift
+			// origArr [0] [1] [2] [3] j
+			// copyArr [1] [2] [3] [0] i
+			
+			shiftedArrL2[j] = origArr[i];
+			
+			if(i == 1){
+				j = origArr.length-1;
+				i = 0;
+				shiftedArrL2[j] = origArr[i];
+			}
+		}
+		
+		// RIGHT SHIFT
+		// AS PER THE ORIGINAL ASSIGNMENT
+		for (int i = 0, j = i+1; j < origArr.length; i++, j++) {
+			// right shift
+			// origArr [0] [1] [2] [3] i
+			// copyArr [3] [0] [1] [2] j
+						
+			shiftedArrR[j] = origArr[i];
+		}
+		
+		shiftedArrR[0] = origArr[origArr.length-1];		
+		System.out.print("LEFT SHIFT 1:	");
+		System.out.println(java.util.Arrays.toString(shiftedArrL1));
+		
+		System.out.print("LEFT SHIFT 2:	");
+		System.out.println(java.util.Arrays.toString(shiftedArrL2));
+		
+		System.out.print("RIGHT SHIFT:	");
+		System.out.println(java.util.Arrays.toString(shiftedArrR));
 	}
 	
 	
@@ -44,16 +91,22 @@ public class Assignment2_1 {
 		System.out.print("You entered array: "); 
 		println(new int[]{1,54,2});
 		
+		System.out.println("---------------");
+		
 		int[] sequence = {1,23,6,5,24,34,54,2};
 		System.out.print("Reversed order: "); 
 		swap(sequence);
+		
+		System.out.println("---------------");
 		
 		int[] sequence2 = {1,62,42,53,4,1,96,5};
 		System.out.print("Copy of your array: ");
 		// IF WITHOUT java.util.Arrays.toString we get [I@15db9742
 		System.out.println(java.util.Arrays.toString(copy(sequence2)));
 		
-		rotate(new int[]{1, 2, 3,4,5,6,7});
+		System.out.println("---------------");
+		
+		rotate(new int[]{30,31,32,33});
 	}
 
 }
