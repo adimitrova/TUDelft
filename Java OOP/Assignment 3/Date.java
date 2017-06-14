64fe817ba09bf56af5ef3505f7627a6af2cd1727
adimitrova <a.dimitrovaa@gmail.com>
2017-06-14 14:02:21 +0200

    beginning of assign.3

---
+++ Java OOP/Assignment 3/Date.java
@@ -0,0 +1,51 @@
+/**
+ * Class representing the current Date
+ * @author Anelia Dimitrova
+ * @version 1
+ *
+ */
+public class Date {
+
+	public static void main(String[] args) {
+		Date today = new Date("13-jun-17");
+		Date tomorrow = new Date("13-jun-17");
+		System.out.println(today.equals(tomorrow));
+	}
+	
+	// fields
+	String date;
+	
+	/**
+	 * class constructor
+	 * initializes the date with the input date from the user
+	 * @param inpDate
+	 */
+	public Date(String inpDate){
+		date = inpDate;
+	}
+	
+	/**
+	 * returns a representation of the current date
+	 */
+	public String toString(){
+		return this.date;
+	}
+	
+	/**
+	 * Compares if two Date objects are the same
+	 * @param other (date object)
+	 */
+	public boolean equals(Object other){
+		boolean equal = false;
+		if(other instanceof Date){
+			Date second = (Date) other;
+			if(this.toString() == second.toString()){
+				equal = true;
+			}
+		}
+		return equal;
+	}
+	
+	
+
+}
---
+++ Java OOP/Assignment 3/DateTest.java
@@ -0,0 +1,25 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.notification.RunListener;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("RedundantStringConstructorCall")
+
+public class DateTest {
+	@Rule
+    public ExpectedException expectedException = ExpectedException.none();
+	
+	@Test
+	public void equals_To_Same_True(){
+		Date date1 = new Date("13-Jun-2017");
+		Date date2 = new Date("13-Jun-2017");
+		assertTrue("date1 and date2 are the same", date1.equals(date2));
+	}
+}
